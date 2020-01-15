72a7994eecd82308a0e668552878fbbace5fc157
David <djg329@drexel.edu>
2020-01-15 18:37:43 -0500

    3-D matplotlibplotting in tkinter window

--- tk_example.py
+++ tk_example.py
@@ -1,77 +1,25 @@
-import random
-from tkinter import Tk, Label, Button, Entry, StringVar, DISABLED, NORMAL, END, W, E
+import tkinter
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.backends.backend_tkagg import (
+                                    FigureCanvasTkAgg, NavigationToolbar2Tk)
+from matplotlib.figure import Figure
 
-class GuessingGame:
-    def __init__(self, master):
-        self.master = master
-        master.title("Guessing Game")
+root = tkinter.Tk()
+root.wm_title("Embedding in Tk")
 
-        self.secret_number = random.randint(1, 100)
-        self.guess = None
-        self.num_guesses = 0
+fig = Figure(figsize=(5, 4), dpi=100)
 
-        self.message = "Guess a number from 1 to 100"
-        self.label_text = StringVar()
-        self.label_text.set(self.message)
-        self.label = Label(master, textvariable=self.label_text)
+canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
+canvas.draw()
 
-        vcmd = master.register(self.validate) # we have to wrap the command
-        self.entry = Entry(master, validate="key", validatecommand=(vcmd, '%P'))
+ax = fig.add_subplot(111, projection="3d")
+t = np.arange(0, 3, .01)
+ax.plot(t, 2 * np.sin(2 * np.pi * t))
 
-        self.guess_button = Button(master, text="Guess", command=self.guess_number)
-        self.reset_button = Button(master, text="Play again", command=self.reset, state=DISABLED)
+toolbar = NavigationToolbar2Tk(canvas, root)
+toolbar.update()
+canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
 
-        self.label.grid(row=0, column=0, columnspan=2, sticky=W+E)
-        self.entry.grid(row=1, column=0, columnspan=2, sticky=W+E)
-        self.guess_button.grid(row=2, column=0)
-        self.reset_button.grid(row=2, column=1)
 
-    def validate(self, new_text):
-        if not new_text: # the field is being cleared
-            self.guess = None
-            return True
-
-        try:
-            guess = int(new_text)
-            if 1 <= guess <= 100:
-                self.guess = guess
-                return True
-            else:
-                return False
-        except ValueError:
-            return False
-
-    def guess_number(self):
-        self.num_guesses += 1
-
-        if self.guess is None:
-            self.message = "Guess a number from 1 to 100"
-
-        elif self.guess == self.secret_number:
-            suffix = '' if self.num_guesses == 1 else 'es'
-            self.message = "Congratulations! You guessed the number after %d guess%s." % (self.num_guesses, suffix)
-            self.guess_button.configure(state=DISABLED)
-            self.reset_button.configure(state=NORMAL)
-
-        elif self.guess < self.secret_number:
-            self.message = "Too low! Guess again!"
-        else:
-            self.message = "Too high! Guess again!"
-
-        self.label_text.set(self.message)
-
-    def reset(self):
-        self.entry.delete(0, END)
-        self.secret_number = random.randint(1, 100)
-        self.guess = 0
-        self.num_guesses = 0
-
-        self.message = "Guess a number from 1 to 100"
-        self.label_text.set(self.message)
-
-        self.guess_button.configure(state=NORMAL)
-        self.reset_button.configure(state=DISABLED)
-
-root = Tk()
-my_gui = GuessingGame(root)
-root.mainloop()
+tkinter.mainloop()
