c934419f4c3ce43f6a0e6dec8a954762af91d900
David <djg329@drexel.edu>
2020-02-17 13:56:01 -0500

    Favicon for site now working

--- rubix_server.py
+++ rubix_server.py
@@ -11,10 +11,21 @@ from flask import Flask , render_template , send_file
 # Defines the server's static paths for serving files
 app = Flask(__name__, static_folder='public', static_url_path='')
 
+
 # Handle the index (home) page
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# Handle any files that begin "/lib" by loading from the lib directory
+@app.route('/lib/<path:path>')
+def send_lib(path):
+    return send_file(Path(app.root_path) / 'lib' / path)
+
+# Handle any unhandled filename by loading its template
+@app.route('/<name>')
+def generic(name):
+    return render_template(name + '.html')
 
 
 if __name__ == "__main__":
