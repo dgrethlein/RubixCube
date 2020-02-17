ee41f513c546ff59aa7d9180d4beaa6f96e82b8a
David <djg329@drexel.edu>
2020-02-17 13:16:09 -0500

    index and base html templates

--- rubix_server.py
+++ rubix_server.py
@@ -1,20 +1,20 @@
 
-
+import os
 import json
 
 import numpy as np
 import pandas as pd
 
 from pathlib import Path
-from flask import Flask
+from flask import Flask , render_template , send_file
 
-
+# Defines the server's static paths for serving files
 app = Flask(__name__, static_folder='public', static_url_path='')
 
-
+# Handle the index (home) page
 @app.route('/')
-def home():
-    return "Hello World!"
+def index():
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
