f58d46a5a8381d17c9963a8f319c84724728d746
David <djg329@drexel.edu>
2020-02-17 20:33:28 -0500

    Converting to side navigation

--- rubix_server.py
+++ rubix_server.py
@@ -6,7 +6,9 @@ import numpy as np
 import pandas as pd
 
 from pathlib import Path
-from flask import Flask , render_template , send_file
+from flask import Flask , jsonify
+from flask import render_template
+from flask import send_file , request
 
 # Defines the server's static paths for serving files
 app = Flask(__name__, static_folder='public', static_url_path='')
@@ -27,6 +29,10 @@ def send_lib(path):
 def generic(name):
     return render_template(name + '.html')
 
+@app.route('/api/test')
+def api_test():
+    clicked = request.args.get('clicked', default='no')
+    return jsonify({'message': 'User clicked ' + clicked})
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8080, debug=True)
